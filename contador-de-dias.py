f09c92c14430c1672a4812c8826ab34bd5ec8bb3
Daniel Amarante <danielamarantedev@gmail.com>
2026-08-23 15:01:52 -0300

    Enhance date calculator with day of the week
    Added functionality to display the day of the week for future and past dates.

--- contador-de-dias.py
+++ contador-de-dias.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from babel.dates import format_date
 from time import sleep
 
 sleep(1)
@@ -8,13 +9,12 @@ sleep(1)
 
 hoje = date.today()
 
-
 while True:
 
     pergunta = input(
         '\nVocê quer calcular dias futuros ou passados? '
         '(F/P): '
-    ).strip().upper()
+    ).strip()[0].upper()
 
     if pergunta == 'F':
         try:
@@ -24,12 +24,14 @@ while True:
             continue
 
         futuro = hoje + timedelta(days=quantos)
+        dia_da_semana_futuro = format_date(futuro, format="EEEE", locale='pt_BR')
+
 
         print('calculando...')
         sleep(2)
         print(
             f'Hoje é {hoje.strftime("%d/%m/%Y")} '
-            f'e daqui a {quantos} dias será {futuro.strftime("%d/%m/%Y")}.'
+            f'e daqui a {quantos} dias será {futuro.strftime("%d/%m/%Y")} - {dia_da_semana_futuro}.'
         )
 
     elif pergunta == 'P':
@@ -40,19 +42,23 @@ while True:
             continue
 
         passado = hoje - timedelta(days=quantos)
+        dia_da_semana_passado = format_date(passado, format="EEEE", locale='pt_BR')
+
         
         print('calculando...')
         sleep(2)
         print(
             f'Hoje é {hoje.strftime("%d/%m/%Y")} '
-            f'e {quantos} dias atrás foi {passado.strftime("%d/%m/%Y")}.'
+            f'e {quantos} dias atrás foi {passado.strftime("%d/%m/%Y")} - {dia_da_semana_passado}.'
         )
 
     else:
         print('Por favor, informe um valor válido: F para futuro ou P para passado.')
 
-    deseja = input('\nDeseja sair? (S/N): ').strip().upper()
+    
+    deseja = str(input('\nDeseja sair? (S/N): ')).strip()[0].upper()
 
-    if deseja == 'S':
+    
+    if deseja != 'N':
         print('Programa encerrado!')
         break
